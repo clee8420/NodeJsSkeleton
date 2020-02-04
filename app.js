494ef5d56b9a179d2a71fa780eeec6a63efce048
codingbabyfood <codingbabyfood@gmail.com>
2020-02-04 02:18:53 -0500

    another setup

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const asyncHandler = fn => (req, res, next) =>
+  Promise
+    .resolve(fn(req, res, next))
+    .catch(next)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/async_sample', async (req, res, next) => {
+  try {
+    // Do something
+    // next(); --> 이거 말고
+    res.send('async_sample')
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
