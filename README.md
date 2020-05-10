c1ab2d9905b88885cabcb09bc04a23daa9f9a263
이재윤 <codingbabyfood@likelion.net>
2020-05-10 19:40:36 +0900

    hololo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NodeJsSkeleton
 This is a Skeleton Code for any NodeJS Server, to be used for projects
+
+sudo lsof -i:[port] will let you know the PID running on specific port
+
+sudo kill -9 [PID] will kill the process with PID(process ID)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ 
+  dest: 'public/images' 
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/image_upload', upload.single('file_to_upload'), function(req, res, next){
+  res.status(200).json({
+    file: req.file,
+    message: "You have successfully uploaded a file"
+  });
+});
+
 
 module.exports = router;
